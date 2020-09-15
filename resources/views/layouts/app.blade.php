210908e4e667badd298684fe70286258aa96d7af
goma.chan <miieooca@gmail.com>
2020-09-15 02:48:20 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,13 @@
 
     <body>
         
-        
         {{-- ナビゲーションバー --}}
         @include('commons.navbar')
 
         <div class="container">
+            {{-- エラーメッセージ --}}
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,6 +3,8 @@
 @extends('layouts.app')
 
 @section('content')
+    
+
 
 <!-- ここにページ毎のコンテンツを書く -->
 
