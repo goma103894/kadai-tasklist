353dc6be728248fffb356cbf1eac3e932225521b
goma.chan <miieooca@gmail.com>
2020-09-15 08:57:56 +0000

    add column

---
+++ database/migrations/2020_09_15_115435_add_status_to_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('status', 10);
+            //最大文字数指定追加
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('status');
+            //
+        });
+    }
+}
